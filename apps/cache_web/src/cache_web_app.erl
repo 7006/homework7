69fb33522e4794da9c79fe43d60b07db46a0dc76
7006 <alexbox86@gmail.com>
2023-10-06 20:21:01 +0300

    Start cache_web app supervisor fisrt

--- apps/cache_web/src/cache_web_app.erl
+++ apps/cache_web/src/cache_web_app.erl
@@ -5,24 +5,22 @@
 -export([start/2, stop/1]).
 
 start(_, _) ->
-    start_cowboy_listener(),
-    cache_web_sup:start_link().
+    cache_web_sup:start_link(),
+    start_cowboy_listener().
 
 stop(_) ->
     stop_cowboy_listener().
 
 start_cowboy_listener() ->
     {ok, Port} = application:get_env(cache_web, port),
+
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/api/cache_server", cache_web_api_h, []}
         ]}
     ]),
-    {ok, _} = cowboy:start_clear(
-        ?MODULE,
-        [{port, Port}],
-        #{env => #{dispatch => Dispatch}}
-    ).
+
+    {ok, _} = cowboy:start_clear(?MODULE, [{port, Port}], #{env => #{dispatch => Dispatch}}).
 
 stop_cowboy_listener() ->
     ok = cowboy:stop_listener(?MODULE).
