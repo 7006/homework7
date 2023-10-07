0992d55c219fc8b3b6f3a4e4d3a8e459dcc9f266
7006 <alexbox86@gmail.com>
2023-10-07 04:55:23 +0300

    Store CreatedAt in cache entry

--- apps/cache_ets/src/cache_ets_table.erl
+++ apps/cache_ets/src/cache_ets_table.erl
@@ -10,25 +10,27 @@
 -define(now, calendar:datetime_to_gregorian_seconds(calendar:universal_time())).
 
 insert(Tab, Key, Val) ->
-    true = ets:insert(Tab, {Key, Val}).
+    CreatedAt = ?now,
+    true = ets:insert(Tab, {Key, Val, CreatedAt}).
 
 insert(Tab, Key, Val, Ttl) when is_integer(Ttl), Ttl > 0 ->
     insert(Tab, Key, Val, Ttl, ?now).
 
 insert(Tab, Key, Val, Ttl, Now) ->
+    CreatedAt = ?now,
     ExpiresAt = Now + Ttl,
-    true = ets:insert(Tab, {Key, Val, ExpiresAt}).
+    true = ets:insert(Tab, {Key, Val, CreatedAt, ExpiresAt}).
 
 lookup(Tab, Key) ->
     lookup(Tab, Key, ?now).
 
 lookup(Tab, Key, Now) ->
     case ets:lookup(Tab, Key) of
-        [{_, Val} | _] ->
+        [{_, Val, _} | _] ->
             Val;
-        [{_, Val, ExpiresAt} | _] when ExpiresAt >= Now ->
+        [{_, Val, _, ExpiresAt} | _] when Now =< ExpiresAt ->
             Val;
-        [{_, _, ExpiresAt} | _] when ExpiresAt < Now ->
+        [{_, _, _, ExpiresAt} | _] when ExpiresAt < Now ->
             undefined;
         [] ->
             undefined
@@ -46,5 +48,5 @@ delete_expired(Tab) ->
     delete_expired(Tab, ?now).
 
 delete_expired(Tab, Now) ->
-    MatchSpec = ets:fun2ms(fun({_, _, ExpiresAt}) when ExpiresAt < Now -> true end),
+    MatchSpec = ets:fun2ms(fun({_, _, _, ExpiresAt}) when ExpiresAt < Now -> true end),
     ets:select_delete(Tab, MatchSpec).
